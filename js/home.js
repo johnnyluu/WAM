93d58cdb20f53f8ce972737e896771493c3af9d3
Zeqing Lu <zeqing.lu@connect.qut.edu.au>
2014-11-21 02:02:45 +1000

    Sort-of parallax

--- js/home.js
+++ js/home.js
@@ -6,4 +6,19 @@ $(window).scroll(function() {
     } else{
     	$("header").addClass("top");
     }
-}); //missing );
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
