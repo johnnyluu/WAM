23b7011fe54410f9d3b587c758b1f60a24a71c95
Zeqing Lu <zeqing.lu@connect.qut.edu.au>
2014-11-22 17:34:32 +1000

    Featured

--- js/home.js
+++ js/home.js
@@ -1,24 +1,46 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
+$(document).ready(function() {
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
 
-    if (scroll >= 550) {
-        $("header").removeClass("top");
-    } else{
-    	$("header").addClass("top");
-    }
-});
+		if (scroll >= 550) {
+			$("header").removeClass("top");
+		} else {
+			$("header").addClass("top");
+		}
+	});
 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
+
+	// $('.has-sub').hover(
+	// 	function() {
+	// 		$(this).addClass('active');
+	// 		$('#subnav').addClass('open');
+	// 	},
+	// 	function() {
+	// 		var on = $(this);
+	// 		$('#subnav').hover(
+	// 			function() {
+	// 				on.addClass('active')
+	// 			},
+	// 			function() {
+	// 				on.removeClass('active')
+	// 			}
+	// 		)
+	// 		$(this).removeClass('active');
+	// 		$('#subnav').removeClass('open');
+	// 	}
+	// )
+})
